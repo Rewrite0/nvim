c2505302a3fb2153a444c2eb53abf71c9865f0f7
Rewrite0 <y747556795@gmail.com>
2023-07-03 16:53:11 +0800

    feat: lualine add codeium status

--- lua/plugin-config/lualine.lua
+++ lua/plugin-config/lualine.lua
@@ -1,29 +1,34 @@
-require('lualine').setup({
-  options = {
-    -- 指定皮肤
-    -- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
-    theme = "auto",
-    -- 分割线
-    component_separators = {
-      left = "|",
-      right = "|",
-    },
-    globalstatus = true,
-  },
-  extensions = { "nvim-tree" },
-  sections = {
-    lualine_x = {
-      "filesize",
-      {
-        "fileformat",
-        symbols = {
-          unix = "LF",
-          dos = "CRLF",
-          mac = "CR",
-        },
-      },
-      "encoding",
-      "filetype",
-    },
-  },
+local function codeium()
+	return "ai:" .. vim.fn["codeium#GetStatusString"]()
+end
+
+require("lualine").setup({
+	options = {
+		-- 指定皮肤
+		-- https://github.com/nvim-lualine/lualine.nvim/blob/master/THEMES.md
+		theme = "auto",
+		-- 分割线
+		component_separators = {
+			left = "|",
+			right = "|",
+		},
+		globalstatus = true,
+	},
+	extensions = { "nvim-tree" },
+	sections = {
+		lualine_x = {
+			{ codeium },
+			"filesize",
+			{
+				"fileformat",
+				symbols = {
+					unix = "LF",
+					dos = "CRLF",
+					mac = "CR",
+				},
+			},
+			"encoding",
+			"filetype",
+		},
+	},
 })
